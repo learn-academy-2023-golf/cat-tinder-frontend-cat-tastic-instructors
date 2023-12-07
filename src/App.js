67fa28d54774b54c7de4fb9aef5cb46ad3192e28
Elyse Montano <elysemontano.dev.com>
2023-12-07 10:24:47 -0800

    adds form and create functionality

--- src/App.js
+++ src/App.js
@@ -16,6 +16,10 @@ const App = () => {
   const [cats, setCats] = useState(mockCats)
   // console.log("cats: ", cats)
 
+  const createCat = (cat) => {
+    console.log("Created cat", cat)
+  }
+
   return(
     <> 
       <Header />
@@ -23,7 +27,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/catindex" element={<CatIndex cats={cats} />} />
         <Route path="/catshow/:id" element={<CatShow cats={cats} />} />
-        <Route path="/catnew" element={<CatNew />} />
+        <Route path="/catnew" element={<CatNew createCat={createCat} />} />
         <Route path="/catedit" element={<CatEdit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
